e62ba077125774abdf35b5b92a224ed0adf07e6d
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-09-25 15:07:11 +0900

    travis ci added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Hello jayden!
+          learn react
         </a>
       </header>
     </div>
